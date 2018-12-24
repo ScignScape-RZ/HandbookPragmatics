9f6826871deff98bd36ed7fc3402bffb07c72bed
ScignScape-RZ <osrworkshops@gmail.com>
2018-12-24 16:11:40 -0500

    update

--- cpp/src/dataset/pdf-pull/pdf-pull-console/main.cpp
+++ cpp/src/dataset/pdf-pull/pdf-pull-console/main.cpp
@@ -14,14 +14,93 @@
 
 #include <QVector>
 
+#include "pin.h"
+
+void test(pinned(int ,x))
+{
+ _repin(x) _as(x + 8);
+ qDebug() << "x = " << nip(x);
+}
+
+void demo1()
+{
+ int a = 9;
+ _pin(int ,x) _tobe(a + 3);
+ a += 20;
+ test(x_pinned);
+}
+
+void demo2()
+{
+ int a = 9;
+
+ _pin(int ,x) _tobe(a + 3);
+
+ int y = nip(x);
+ _repin(x) _as(x + 5);
+
+ qDebug() << "y = " << y;
+ qDebug() << "x = " << nip(x);
+}
+
+void demo3()
+{
+ int a = 9;
+
+ _pin(int ,x) _tobe(a + 3);
+ _pin(int ,y) _tobe(a + 4);
+
+ _repin(x) _as(x + 5);
+
+ _repin(x)
+   x += nip(y);
+ _as(x);
+
+ _nip(x)
+ {
+  _nip(y)
+  {
+   qDebug() << "y = " << y;
+   qDebug() << "x = " << x;
+  };
+ };
+}
+
+void test1(pinned(int ,x))
+{
+ _unpin(x);
+ x += 50;
+ qDebug() << "x1 = " << x;
+}
+
+void demo4()
+{
+ int a = 9;
+ _pin(int ,x) _tobe(a + 3);
+ a += 20;
+ test1(x_pinned);
+
+ qDebug() << "x = " << nip(x);
+}
 
 int main(int argc, char *argv[])
 {
- QApplication qapp(argc, argv);
- add_to_data_set("test", 1);
-// QLispConsole* main_console = QLispConsole::getInstance();
-// QLispConsole_Dialog* qld = new QLispConsole_Dialog("", main_console);
-// qld->show();
- qapp.exec();
+ demo1();
+ demo2();
+ demo3();
+ demo4();
+
  return 0;
 }
+
+
+//int main(int argc, char *argv[])
+//{
+// QApplication qapp(argc, argv);
+// add_to_data_set("test", 1);
+//// QLispConsole* main_console = QLispConsole::getInstance();
+//// QLispConsole_Dialog* qld = new QLispConsole_Dialog("", main_console);
+//// qld->show();
+// qapp.exec();
+// return 0;
+//}
